129b426f579276c015f50f92f78362fb7d91955a
Thiago Soares <tcruz_estag@horizonte.tv.br>
2015-01-27 17:30:41 -0200

    Modificações feitas no template do admin

--- polls/admin.py
+++ polls/admin.py
@@ -4,14 +4,22 @@ from django.contrib import admin
 from polls.models import Choice
 from polls.models import Question
 
-        # Faz a adição dos campos de choices de forma linear, ao contrário do outro  
+
+        # Faz a adição dos campos de choice no add question de forma empilhada, ao contrário do outro  
         # código que só me permitia fazer a adição de um só campo; este tipo de código 
         # permite que eu possa fazer adições em série para o meu database;
 
-class ChoiceInline(admin.StackedInline):
+
+        # Este trecho de código abaixo permite fazer a amostragem das choices - escolhas
+        # de forma empilhada; a outra modificação implementada permite fazer de forma 
+        # tabular 
+        # class ChoiceInline(admin.StackedInline):
+
+
+class ChoiceInline(admin.TabularInline):
+
     model = Choice
     extra = 3
-
 
 
 class QuestionAdmin(admin.ModelAdmin):
@@ -23,6 +31,15 @@ class QuestionAdmin(admin.ModelAdmin):
         # Esse tipo de classe faz o hide da informação a ela atrelada no field, nesse
         # caso ao field Date information
     ]
+   
+    # Faz a modifição de como é exibido a página do código de selecionar a questão
+    # Exibe o template em forma de colunas com a data da publicação e se foi publicado 
+    # recentemente, se for retirado essa linha ele vai exibir de forma empilhada e sem as
+    # informações adicionais
+   
+
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    
     inlines = [ChoiceInline]
 # admin.site.register(Question)
 
